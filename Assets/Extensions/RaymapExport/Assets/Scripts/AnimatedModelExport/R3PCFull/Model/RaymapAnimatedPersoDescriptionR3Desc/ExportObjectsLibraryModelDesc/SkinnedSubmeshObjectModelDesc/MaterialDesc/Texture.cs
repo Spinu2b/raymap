b2cda5d8daf985a5fa854394ad770ed8fe08d0f5
Spinu2b <spinu2b@gmail.com>
2020-03-17 00:28:39 +0100

    Converting Unity materials to custom materials model for export

--- Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/R3PCFull/Model/RaymapAnimatedPersoDescriptionR3Desc/ExportObjectsLibraryModelDesc/SkinnedSubmeshObjectModelDesc/MaterialDesc/Texture.cs
+++ Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/R3PCFull/Model/RaymapAnimatedPersoDescriptionR3Desc/ExportObjectsLibraryModelDesc/SkinnedSubmeshObjectModelDesc/MaterialDesc/Texture.cs
@@ -12,6 +12,14 @@ namespace Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.R3PC
         public float green;
         public float blue;
         public float alpha;
+
+        public Color(float red, float green, float blue, float alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
     }
 
     public class Texture
