655d34c74bd07d17a67f1c3e0f5bf06e649fec09
Spinu2b <spinu2b@gmail.com>
2020-03-25 18:40:44 +0100

    Development

---
+++ Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/ModelManipulation/RaymapAnimatedPersoExporter.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.ModelManipulation
+{
+    public class RaymapAnimatedPersoExporter
+    {
+        public RaymapAnimatedPersoDescription Export(GameObject gameObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/RaymapExportPersoComponent.cs
+++ Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/RaymapExportPersoComponent.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.Model;
+using Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.ModelManipulation;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,12 @@ namespace Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport
 {
     public class RaymapExportPersoComponent : MonoBehaviour
     {
+        public void ExportModelWithAnimations(string outputFilePath)
+        {
+            var exporter = new RaymapAnimatedPersoExporter();
+            RaymapAnimatedPersoDescription result = exporter.Export(gameObject);
+            var fileContent = JsonConvert.SerializeObject(result);
+            File.WriteAllText(outputFilePath, fileContent);
+        }
     }
 }
