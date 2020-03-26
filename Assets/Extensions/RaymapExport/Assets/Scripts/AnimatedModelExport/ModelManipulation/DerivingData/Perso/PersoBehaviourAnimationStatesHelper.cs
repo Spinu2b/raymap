aaac368e30b3aeb56541ca29158ff80d91703f37
Spinu2b <spinu2b@gmail.com>
2020-03-26 21:07:53 +0100

    Development, base for iterating Perso states

--- Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/ModelManipulation/DerivingData/Perso/PersoBehaviourAnimationStatesHelper.cs
+++ Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/ModelManipulation/DerivingData/Perso/PersoBehaviourAnimationStatesHelper.cs
@@ -8,5 +8,76 @@ namespace Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.Mode
 {
     public class PersoBehaviourAnimationStatesHelper
     {
+        public PersoBehaviourInterface persoBehaviourInterface { get; private set; }
+        private int currentPersoAnimationStateIndex = 0;
+
+        public PersoBehaviourAnimationStatesHelper(PersoBehaviourInterface persoBehaviourInterface)
+        {
+            this.persoBehaviourInterface = persoBehaviourInterface;
+        }
+
+        public void SwitchToFirstAnimationState()
+        {
+            SwitchContextToAnimationStateOfIndex(GetFirstPersoStateIndex());
+            currentPersoAnimationStateIndex = GetFirstPersoStateIndex();
+        }
+
+        private int GetFirstPersoStateIndex()
+        {
+            return 0;
+        }
+
+        private void SwitchContextToAnimationStateOfIndex(int stateIndex)
+        {
+            persoBehaviourInterface.SetState(stateIndex);
+            currentPersoAnimationStateIndex = stateIndex;
+        }
+
+        public bool AreValidPersoAnimationStatesLeftIncludingCurrentOne()
+        {
+            return currentPersoAnimationStateIndex < persoBehaviourInterface.statesCount;
+        }
+
+        public bool AreFramesLeftForCurrentAnimationStateStartingWithFrameNumber(int currentFrameNumber)
+        {
+            return currentFrameNumber < persoBehaviourInterface.currentAnimationStateFramesCount;
+        }
+
+        public int GetStateAnimationNextFrameNumberAfter(int currentFrameNumber)
+        {
+            return currentFrameNumber + 1;
+        }
+
+        public int GetCurrentPersoStateIndex()
+        {
+            return currentPersoAnimationStateIndex;
+        }
+
+        public void AcquireNextValidPersoAnimationState()
+        {
+            int currentStateIndex = currentPersoAnimationStateIndex;
+            currentStateIndex++;
+
+            while (!IsValidPersoAnimationState(currentStateIndex))
+            {
+                currentStateIndex++;
+                if (currentStateIndex >= persoBehaviourInterface.statesCount)
+                {
+                    currentPersoAnimationStateIndex = currentStateIndex;
+                    return;
+                }
+            }
+            SwitchContextToAnimationStateOfIndex(currentStateIndex);
+        }
+
+        private bool IsValidPersoAnimationState(int animationStateIndex)
+        {
+            return persoBehaviourInterface.IsValidAnimationState(animationStateIndex);
+        }
+
+        public int GetFirstValidStateAnimationKeyframeFrameNumber()
+        {
+            return 1;
+        }
     }
 }
--- Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/ModelManipulation/DerivingData/PersoAnimationStatesGeneralDataManipulator.cs
+++ Assets/Extensions/RaymapExport/Assets/Scripts/AnimatedModelExport/ModelManipulation/DerivingData/PersoAnimationStatesGeneralDataManipulator.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.ModelManipulation.DerivingData.Model;
+using Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.ModelManipulation.DerivingData.Perso;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,44 @@ namespace Assets.Extensions.RaymapExport.Assets.Scripts.AnimatedModelExport.Mode
         public IEnumerable<AnimationStateGeneralInfo> IterateAnimationStatesGeneralDataForExport(GameObject persoGameObject)
         {
             throw new NotImplementedException();
+
+            var persoBehaviourAnimationStatesHelper = new PersoBehaviourAnimationStatesHelper(GetPersoBehaviourFor(persoGameObject));
+            persoBehaviourAnimationStatesHelper.SwitchToFirstAnimationState();
+            while (persoBehaviourAnimationStatesHelper.AreValidPersoAnimationStatesLeftIncludingCurrentOne())
+            {
+                
+                /* * /
+                var animationClip = new AnimationClipModel(persoBehaviourAnimationStatesHelper.GetCurrentAnimationClipName());
+                while (persoBehaviourAnimationStatesHelper.AreAnimationFramesLeft())
+                {
+                    var animTreeWithChannelsDataHierarchy = persoBehaviourAnimationStatesHelper.DeriveAnimTreeWithChannelsDataHierarchyForGivenFrame(
+                    persoBehaviourAnimationStatesHelper.GetCurrentFrameNumberForExport());
+
+                    var animationFrame = new AnimationFrameModel(persoBehaviourAnimationStatesHelper.GetCurrentFrameNumberForExport());
+                    animationClip.AddKeyframe(animationFrame.index, animationFrame);
+                    persoBehaviourAnimationStatesHelper.NextFrame();
+                }
+                yield return animationClip;
+                /* */
+                persoBehaviourAnimationStatesHelper.AcquireNextValidPersoAnimationState();
+                
+            }
+        }
+
+        private PersoBehaviourInterface GetPersoBehaviourFor(GameObject persoGameObject)
+        {
+            if (persoGameObject.GetComponent<PersoBehaviour>() != null)
+            {
+                return new PersoBehaviourInterface(persoGameObject.GetComponent<PersoBehaviour>());
+            }
+            else if (persoGameObject.GetComponent<ROMPersoBehaviour>() != null)
+            {
+                return new PersoBehaviourInterface(persoGameObject.GetComponent<ROMPersoBehaviour>());
+            }
+            else
+            {
+                throw new InvalidOperationException("This game object does not have any Perso Behaviour component!");
+            }
         }
     }
 }
