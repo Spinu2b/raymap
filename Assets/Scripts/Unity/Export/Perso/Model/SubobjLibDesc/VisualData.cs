b9e25e31b37569859ed3fdc141314a047fa9f4f9
Spinu2b <spinu2b@gmail.com>
2020-07-22 19:52:15 +0200

    Visual data model initial code, updated .gitignore

--- Assets/Scripts/Unity/Export/Perso/Model/SubobjLibDesc/VisualData.cs
+++ Assets/Scripts/Unity/Export/Perso/Model/SubobjLibDesc/VisualData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts.Unity.Export.Perso.Model.SubobjLibDesc.VisDatDesc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Assets.Scripts.Unity.Export.Perso.Model.SubobjLibDesc
 {
     public class VisualData
     {
+        public Dictionary<string, Material> materials = new Dictionary<string, Material>();
+        public Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
+        public Dictionary<string, Image> images = new Dictionary<string, Image>();
     }
 }
