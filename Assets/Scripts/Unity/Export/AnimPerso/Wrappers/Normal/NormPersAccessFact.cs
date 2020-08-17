e78021e8e6e27b36b2b15c07168503d8e6f4feae
Spinu2b <spinu2b@gmail.com>
2020-08-17 23:16:39 +0200

    Normal perso accessor factory implementation initial code

--- Assets/Scripts/Unity/Export/AnimPerso/Wrappers/Normal/NormPersAccessFact.cs
+++ Assets/Scripts/Unity/Export/AnimPerso/Wrappers/Normal/NormPersAccessFact.cs
@@ -13,7 +13,44 @@ namespace Assets.Scripts.Unity.Export.AnimPerso.Wrappers.Normal
         public static NormalPersoAccessor FromPersoGameObject(GameObject persoGameObject)
         {
             var result = new NormalPersoAccessor();
+            result.name = persoGameObject.name;
+
+            PersoBehaviour persoBehaviour = persoGameObject.GetComponent<PersoBehaviour>();
+
+            result.isLoaded = persoBehaviour.IsLoaded;
+            result.perso = persoBehaviour.perso;
+
+            result.animLargo = persoBehaviour.animLargo;
+            result.animMontreal = persoBehaviour.animMontreal;
+            result.a3d = persoBehaviour.a3d;
+
+            result.subObjects = GetSubobjects(persoBehaviour);
+            result.channelObjects = GetChannelObjects(persoBehaviour);
+
+            result.poListIndex = persoBehaviour.poListIndex;
+            result.morphDataArray = persoBehaviour.morphDataArray;
+
+            result.hasBones = (bool)persoBehaviour.GetType().GetField(
+                "hasBones", System.Reflection.BindingFlags.NonPublic | BindingFlags.Instance).GetValue(persoBehaviour);
+
+            result.channelIDDictionary = CloneChannelIDDictionary(persoBehaviour);
+
+
             throw new NotImplementedException();
+        }
+
+        private static Dictionary<short, List<int>> CloneChannelIDDictionary(PersoBehaviour persoBehaviour)
+        {
+            var result = new Dictionary<short, List<int>>();
+
+            var originalChannelIDDictionary = (Dictionary<short, List<int>>)persoBehaviour.GetType().GetField(
+                "channelIDDictionary", System.Reflection.BindingFlags.NonPublic | BindingFlags.Instance).GetValue(persoBehaviour);
+
+            foreach (var sublistKey in originalChannelIDDictionary.Keys)
+            {
+                result[sublistKey] = originalChannelIDDictionary[sublistKey].Select(x => x).ToList();
+            }
+            return result;
         }
     }
 }
