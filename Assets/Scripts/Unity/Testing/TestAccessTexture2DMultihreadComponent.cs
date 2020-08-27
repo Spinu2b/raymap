6a988a6ae429cc0e3ac12dd87a5ce9b9645f64fd
Spinu2b <spinu2b@gmail.com>
2020-08-27 23:48:08 +0200

    Code for testing access to Texture2D objects from another threads - as expected - it fails, throws exception

---
+++ Assets/Scripts/Unity/Testing/TestAccessTexture2DMultihreadComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Unity.Testing
+{
+    public class TestAccessTexture2DMultihreadComponent : MonoBehaviour
+    {
+        public static void ThreadAccessTexture2DGetPixels(object texture2DObject)
+        {
+            Texture2D texture = texture2DObject as Texture2D;
+            texture.GetPixels();
+        }
+
+        public void TestAccessingTexture2DGetPixelsMethodFromDifferentThreads()
+        {
+            var texture = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+
+            // set the pixel values
+            texture.SetPixel(0, 0, new Color(1.0f, 1.0f, 1.0f, 0.5f));
+            texture.SetPixel(1, 0, Color.clear);
+            texture.SetPixel(0, 1, Color.white);
+            texture.SetPixel(1, 1, Color.black);
+
+            // Apply all SetPixel calls
+            texture.Apply();
+
+            Thread t = new Thread(ThreadAccessTexture2DGetPixels);
+            t.Start(texture);
+            t.Join();
+        }
+    }
+}
