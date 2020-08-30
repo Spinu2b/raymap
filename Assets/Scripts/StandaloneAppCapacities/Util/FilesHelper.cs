97ed0ca096e85ea16921add7c6bebdeeef372b6a
Spinu2b <spinu2b@gmail.com>
2020-08-31 00:11:52 +0200

    Removed unneeded code, moved some utils to new assembly

---
+++ Assets/Scripts/StandaloneAppCapacities/Util/FilesHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.StandaloneAppCapacities.Util
+{
+    public static class FilesHelper
+    {
+        public static string AvoidNotAllowedCharsInFilepath(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
